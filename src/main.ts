421802a95d8cd6822ac23344553054e918731c41
Christian Pengu <pengu@goinfoteam.it>
2023-10-25 19:43:01 +0200

    log

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('SERVER START ON PORT: ', 3000);
 }
 bootstrap();
